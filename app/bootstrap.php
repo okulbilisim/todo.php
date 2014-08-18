d55ceaf7537c44b28ae41d4f12f136c7109f6144
hasantayyar <tayyar.besik@gmail.com>
2014-08-19 01:14:37 +0300

    starting angular way app.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,24 +7,6 @@ define('APP_ROOT_PATH', __DIR__."/../");
 $app = new \Slim\Slim(
         array(
     'cookies.encrypt' => true,
-    'cookies.secret_key' => 'Kz7ac#wmx.11',
+    'cookies.secret_key' => 'kazant@#$.12',
     'templates.path' => APP_ROOT_PATH . '/app/views/',
         ));
-
-$app->container->singleton('twig', function ($c) {
-    $twig = new \Slim\Views\Twig();
-    /* Option */
-    $twig->parserOptions = array(
-        'debug' => true,
-        'cache' => APP_ROOT_PATH . '/cache',
-        'auto_reload' => true,
-        'strict_variables' => false,
-        'autoescape' => true
-    );
-    /* Extensions */
-    $twig->parserExtensions = array(new \Slim\Views\TwigExtension());
-    $templatesPath = $c['settings']['templates.path'];
-    $twig->setTemplatesDirectory($templatesPath);
-    return $twig;
-});
-
--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 include __DIR__ . "/../app/bootstrap.php";
-$app->twig->display('index.twig');
+    $app->render('index.php');
+
 $app->get('/', function() use ($app) {
-    $app->twig->display('index.twig');
+    $app->render('index.php');
 });
+
+
